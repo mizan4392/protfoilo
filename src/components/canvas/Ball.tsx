6b050b0cdf947386b63abf50faaaf5f2e4e070a5
green <macbook@macs-MacBook-Pro.local>
2024-09-07 01:35:41 +0200

    tech section

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import { CanvasLoader } from "..";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import { Suspense } from "react";
 
-const Ball = () => {
+type BallProps = {
+  icon: string;
+};
+const Ball = ({ icon }: BallProps) => {
+  const [decal] = useTexture([icon]);
   return (
-    <div>Ball</div>
-  )
-}
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color={"#ff8eb"}
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          map={decal}
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+        />
+      </mesh>
+    </Float>
+  );
+};
+const BallCanvas = ({ icon }: BallProps) => {
+  return (
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball icon={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
 
-export default Ball
+export default BallCanvas;
